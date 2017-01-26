ad32896cbbdf2c57d8cc8b79cd4765c0fcf94877
Boris D <boris.dob@gmail.com>
2017-01-25 20:20:22 -0800

    Render React element to string.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require('http')
 const React = require('react')
+const ReactDOMServer = require('react-dom/server')
 
 const server = http.createServer()
 
+const text = 'hello world'
+
 server.on('request', (request, response) => {
-  response.end('hello world')
+  const element = React.createElement('p', null, text)
+  const str = ReactDOMServer.renderToString(element)
+  response.end(str)
 })
 
 server.listen(3000)
