8a9f7ae2a45b5f96d68441101d83300ea756b262
Boris D <boris.dob@gmail.com>
2017-01-25 20:41:03 -0800

    Refactor.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const http = require('http')
 const React = require('react')
 const ReactDOMServer = require('react-dom/server')
 
-const server = http.createServer()
-
 function tokenizeText (text) {
   return text
     .split(/\s+/)
@@ -16,12 +14,14 @@ function intersperse (text, delimiter) {
   return [].concat(...text.map(t => [delimiter, t])).slice(1)
 }
 
-server.on('request', (request, response) => {
+function requestHandler (request, response) {
   const text = 'hello beautiful world'
   const children = intersperse(tokenizeText(text), ' ')
   const element = React.createElement('p', null, ...children)
   const str = ReactDOMServer.renderToString(element)
   response.end(str)
-})
+}
 
+const server = http.createServer()
+server.on('request', requestHandler)
 server.listen(3000)
